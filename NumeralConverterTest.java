26e56701024de384a6e2b7c0edb60f54accb3a30
Drew Keller <drew@eightycolumns.com>
2016-06-09 15:33:35 -0400

    Test conversion of MCMLXXIX to 1979

--- NumeralConverterTest.java
+++ NumeralConverterTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 public class NumeralConverterTest {
   @Test
+  public void upperCaseRomanNumeralMCMLXXIXConvertsToArabicNumeral1979() {
+    assertEquals("1979", NumeralConverter.romanToArabic("MCMLXXIX"));
+  }
+
+  @Test
   public void upperCaseRomanNumeralMMMCMXCIXConvertsToArabicNumeral3999() {
     assertEquals("3999", NumeralConverter.romanToArabic("MMMCMXCIX"));
   }
